fe067dcdba76220c1d5f5e438c6fd4abb80354c6
고세종 <fulfilled7418@gmail.com>
2023-12-03 14:11:52 +0900

    feat: 구현 Lotto getter

--- javascript-lotto-6/src/Lotto.js
+++ javascript-lotto-6/src/Lotto.js
@@ -10,9 +10,20 @@ class Lotto {
     if (numbers.length !== 6) {
       throw new Error('[ERROR] 로또 번호는 6개여야 합니다.');
     }
+    const check = /^[0-9]+$/;
+    if (check.test(numbers)) {
+      throw new Error('[ERROR] 숫자를 입력해 주세요.');
+    }
+    if (numbers.every((number) => number >= 1 && number <= 45)) {
+      throw new Error('[ERROR] 1 ~ 45 사이의 숫자를 입력해주세요.');
+    }
   }
 
   // TODO: 추가 기능 구현
+
+  getNumber() {
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
